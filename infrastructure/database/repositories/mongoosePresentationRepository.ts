afde51e1b9340caf6c88774fe15bf07ea6c8ff5c
amalabraham93 <amalabraham93@gmail.com>
2023-07-21 13:09:30 +0530

    creating conference error

--- infrastructure/database/repositories/mongoosePresentationRepository.ts
+++ infrastructure/database/repositories/mongoosePresentationRepository.ts
@@ -22,10 +22,37 @@ const PresentationModel = mongoose.model<Presentation>(
 );
 
 class MongoosePresentationRepository implements PresentationRepository {
+
+
+  
   async createPresentation(presentation: Presentation): Promise<Presentation> {
-    const createdPresentation = await PresentationModel.create(presentation);
-    return createdPresentation.toObject();
+    const { conference } = presentation;
+  
+    try {
+      // Try to find an existing presentation with the same conferenceId
+      let existingPresentation = await PresentationModel.findOne({ conference });
+  
+      if (existingPresentation) {
+        // If an existing presentation is found, update it with the new data
+        existingPresentation = await PresentationModel.findOneAndUpdate(
+          { conference },
+          presentation,
+          { new: true } // Setting "new" to true returns the updated document
+        );
+        return existingPresentation!.toObject();
+      } else {
+        // If no existing presentation is found, create a new one
+        const createdPresentation = await PresentationModel.create(presentation);
+        return createdPresentation.toObject();
+      }
+    } catch (error) {
+      // Handle errors
+      throw new Error("Error creating/updating presentation");
+    }
   }
+
+
+
 
   async getPresentationById(_id: ObjectId): Promise<Presentation | null> {
     const foundPresentation = await PresentationModel.findById(_id).exec();
